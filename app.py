e8fb6c3fbe2b4a4d50a7eedab04fc5a4b2c480c3
Faizan Tanveer <faizantanwir02@gmail.com>
2025-09-27 16:56:12 +0300

    Imported a function, i.e. abort from the Flask library.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash, g
+from flask import Flask, render_template, request, redirect, url_for, session, flash, g, abort
 from werkzeug.security import generate_password_hash, check_password_hash
 from cryptography.fernet import Fernet
 import collections
